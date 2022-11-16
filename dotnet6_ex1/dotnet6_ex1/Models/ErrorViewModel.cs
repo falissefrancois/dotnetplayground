13775a2416b17eb743eb1e0961e90f7e94a85253
francois <falisse.francois@gmail.com>
2022-11-16 14:31:33 +0100

    New Sample dotnet 6 MVP Web App

---
+++ dotnet6_ex1/dotnet6_ex1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnet6_ex1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
